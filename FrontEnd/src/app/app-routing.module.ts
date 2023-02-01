a69c0c0d9743ea8cba5e9426ea3938544433fa5e
EddyJavs <eddjavs@gmail.com>
2023-01-31 22:19:33 -0600

    Se agrega barra de navegacion

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './page/menu/menu.component';
+import { HomeComponent } from './page/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: '**', pathMatch:'full', redirectTo: 'home'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { MenuComponent } from './page/menu/menu.component';
+import { NavComponent } from './page/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
